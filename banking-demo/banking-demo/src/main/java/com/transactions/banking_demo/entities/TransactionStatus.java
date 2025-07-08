30a85e5453547250ea4ef8d7eb94720dfc222695
jarivera. <jarivera@bancolombia.com.co>
2025-07-07 19:44:40 -0500

    Se elimina codigo viejo de otros cursos

--- banking-demo/banking-demo/src/main/java/com/transactions/banking_demo/entities/TransactionStatus.java
+++
@@ -1,5 +0,0 @@
-package com.transactions.banking_demo.entities;
-
-public enum TransactionStatus {
-    PENDING, POSTED, FAILED
-}
--- banking-demo/banking-demo/src/main/java/com/transactions/banking_demo/entities/TransactionType.java
+++
@@ -1,5 +0,0 @@
-package com.transactions.banking_demo.entities;
-
-public enum TransactionType {
-    CASH_IN, CASH_OUT
-}
